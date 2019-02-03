645f1e11ef979ca5929fa2f97524acca69ab1240
Ralf Junghanns <ralf.junghanns@gmail.com>
2019-02-03 20:19:38 +0100

    Add JWT-authentication

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -9,4 +9,5 @@ return [
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
